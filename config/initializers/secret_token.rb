d69471df121226983a61e9b92ae11b4d709759f1
root <root@Mark-II.(none)>
2014-04-14 09:09:51 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arthmitee::Application.config.secret_key_base = '9f11051106e4c206768f5504dbe1e04f476c5767c6f8996843cabd65ffa198024a0e8180b7edcebc719579728fe55372cd3c61e69ea07644c28e1e9cfc0cffc6'
